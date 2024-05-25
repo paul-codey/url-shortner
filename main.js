e5af55827a8f8c1014feb227400f2eca36c67fa8
Peboy <pauldcoder@gmail.com>
2024-05-25 13:34:41 +0100

    update

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const shorteningBtn = document.querySelector(".shortening-box__btn");
 const input = document.querySelector(".shortening-box__input");
 const shortenBox = document.querySelector(".shortened-links");
 
+const emptyInput = document.querySelector(".empty--input");
+
 function renderhtml(input) {
   const html = `
   <div class="link flex">
@@ -21,16 +23,49 @@ function renderhtml(input) {
   shortenBox.insertAdjacentHTML("beforeend", html);
 }
 
+
+
 shorteningBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (input.value) {
-    renderhtml(input.value);
+    renderhtml(input.value.trim());
   } else {
     input.classList.add("empty-input");
-    document.querySelector(".empty--input").classList.add("show");
+    emptyInput.classList.add("show");
   }
 });
 
 document.addEventListener("click", (e) => {
-  if (e.target !== shorteningBtn) input.classList.remove("empty-input");
+  if (e.target !== shorteningBtn) {
+    input.classList.remove("empty-input");
+
+    emptyInput.classList.remove('show');
+  }
+
 });
+
+
+async function shortenUrl(longURL) {
+  try {
+    const url = new URL(longURL);
+    console.log(url);
+  } catch (e) {
+    throw new Error('Invalid link')
+  }
+
+  const encoded = encodeURIComponent(longURL);
+
+  const response = await fetch('https://cors-anywhere.herokuapp.com/https://cleanuri.com/api/v1/shorten', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ url: encoded }),
+  });
+  
+  const data = await response.json();
+  console.log(data);
+}
+
+
+shortenUrl('https://mimo.app/i/beach.png');
